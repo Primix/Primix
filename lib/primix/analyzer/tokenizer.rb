2ef2d70ee35095d44498b712f02c7eff809e5059
Draveness <stark.draven@gmail.com>
2016-09-22 16:51:12 +0800

    Reindent methods in tokenizer

--- lib/primix/analyzer/tokenizer.rb
+++ lib/primix/analyzer/tokenizer.rb
@@ -18,17 +18,17 @@ module Primix
         current_token = ""
 
         chars.each_with_index do |char, index|
-            case char
-            when "(", ")", "{", "}", "[", "]", ":", "=", "\"", "-", ">", "." then
-                tokens << current_token if current_token != ""
-                tokens << char
-                current_token = ""
-            when " ", "\n", "\t" then
-                tokens << current_token if current_token != ""
-                current_token = ""
-            else
-                current_token << char
-            end
+          case char
+          when "(", ")", "{", "}", "[", "]", ":", "=", "\"", "-", ">", "." then
+            tokens << current_token if current_token != ""
+            tokens << char
+            current_token = ""
+          when " ", "\n", "\t" then
+            tokens << current_token if current_token != ""
+            current_token = ""
+          else
+            current_token << char
+          end
         end
         tokens
       end
