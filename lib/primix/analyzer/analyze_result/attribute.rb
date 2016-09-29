adeaa538284cefb3cfc534d2b6f63e1e73374edf
Draveness <stark.draven@gmail.com>
2016-09-30 00:01:48 +0800

    Add attribute and initialize method to attribute

--- lib/primix/analyzer/analyze_result/attribute.rb
+++ lib/primix/analyzer/analyze_result/attribute.rb
@@ -2,7 +2,13 @@ module Primix
   class Analyzer
     class AnalyzeResult
       class Attribute
+        attr_reader :name
+        attr_reader :value
 
+        def initialize(name, value)
+          @name = name
+          @value = value
+        end
       end
     end
   end
