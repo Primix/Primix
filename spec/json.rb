66d629033c4830f3e4d0f05ab529c2501057aa17
Draveness <stark.draven@gmail.com>
2016-10-09 21:37:22 +0800

    Fixed conflict in json

--- spec/json.rb
+++ spec/json.rb
@@ -8,13 +8,18 @@ class Json < Primix::Annotation
     static func parse(json: Any) -> #{meta.name}? {
         guard let json = json as? [String: Any] else { return nil }
         guard #{json_extraction_lists.join(",\n\t\t")} else { return nil }
-        return #{meta.name}()
+        return #{meta.name}(#{meta.attributes.map { |a| "#{a.name}: #{a.name}" }.join(", ") })
     }
 }"""
   end
 
   private
 
+  def json_extraction_lists
+    meta.attributes.map do |attr|
+      "let #{attr.name} = json[\"#{attr.name}\"] as? #{attr.typename}"
+    end
+  end
 end
 
 
