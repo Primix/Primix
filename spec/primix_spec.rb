e960cab9e4ad936d8ebb20a92c439b436f07adc8
Draveness <stark.draven@gmail.com>
2016-10-21 17:44:51 +0800

    Update primix major spec file

--- spec/primix_spec.rb
+++ spec/primix_spec.rb
@@ -5,12 +5,6 @@ describe Primix::Analyzer, "#analyze!" do
 
   it "runs" do
     analyzer = Primix::Analyzer.new(".")
-    file_klass_hash = analyzer.analyze!
-
-    file_klass_hash.each do |_, klass|
-      meta = klass
-      # puts Json.new(meta).generate!
-    end
-    # puts file_klass_hash
+    analyzer.analyze!
   end
 end
