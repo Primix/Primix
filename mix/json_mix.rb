eb6f09af4e2eef6582c1ed01ba05bee97166e340
Draveness <stark.draven@gmail.com>
2016-10-24 12:36:03 +0800

    Add json nested function

--- mix/json_mix.rb
+++ mix/json_mix.rb
@@ -20,20 +20,32 @@ class Json < Primix::Processor
       transformer = meta.functions.detect do |func|
         func.name == "#{attr.name}Transformer" && func.is_static_method?
       end
-      json_key_path = json_property_hash[attr.name] || attr.name
+      extract_from_json = extract_from_json(attr)
       if transformer
         transformer_from_type = transformer.param_types.first
         if transformer_from_type == "Any"
-          "let #{attr.name} = json[\"#{json_key_path}\"].flatMap(#{meta.name}.#{transformer.name})"
+          "let #{attr.name} = #{extract_from_json}.flatMap(#{meta.name}.#{transformer.name})"
         else
-          "let #{attr.name} = (json[\"#{json_key_path}\"] as? #{transformer_from_type}).flatMap(#{meta.name}.#{transformer.name})"
+          "let #{attr.name} = (#{extract_from_json} as? #{transformer_from_type}).flatMap(#{meta.name}.#{transformer.name})"
         end
       else
-        "let #{attr.name} = json[\"#{json_key_path}\"] as? #{attr.typename}"
+        "let #{attr.name} = #{extract_from_json} as? #{attr.typename}"
       end
     end
   end
-
+  def extract_from_json(attr)
+    json_key_path = json_property_hash[attr.name] || attr.name
+    key_paths = json_key_path.split(".")
+    result = "json"
+    key_paths.each_with_index do |key_path, index|
+      if index == key_paths.count - 1
+        result += "[\"#{key_path}\"]"
+      else
+        result = "(#{result}[\"#{key_path}\"] as? [String: Any])?"
+      end
+    end
+    result
+  end
   def json_property_hash
     meta.attributes.detect { |a| a.name == "JSONKeyPathByPropertyKey" && a.is_static_attr? }.default_value
   end
